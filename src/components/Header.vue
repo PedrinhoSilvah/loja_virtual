310b0331046f2444c70f224ba62e0874a7d7405f
Pedro Miguel <pedrinhosillvah59@gmail.com>
2024-03-19 21:48:08 -0300

    construindo o header

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+</script>
+
+<template>
+    <header class="header">
+        <nav class="nav">
+                <a class="nav-item">Pc gamer</a>
+                <a class="nav-item">Celulares</a>
+                <a class="nav-item">Mouses</a>
+                <a class="nav-item">TV</a>
+                <a class="nav-item">Monitores</a>
+                <a class="nav-item">Relógios</a>
+        </nav>
+         <img class="Logo-img"
+            src='https://static.vecteezy.com/ti/vetor-gratis/p2/2734090-shopping-cart-icon-vector-design-gr%C3%A1tis-vetor.jpg'
+            alt="logo">
+    </header>
+</template>
+
+
+
+<style src="src/components/Header.scss" lang="scss" scoped>
+
+</style>
